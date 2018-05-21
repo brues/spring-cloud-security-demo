3133b05675fe311389e61b189c6048e81748e3e2
guoyibin <1979696310@qq.com>
2018-05-21 20:33:23 +0800

    完善接口定义

--- common/src/main/java/com/blues/security/common/DatabaseApi.java
+++ common/src/main/java/com/blues/security/common/DatabaseApi.java
@@ -1,13 +1,25 @@
-package com.blues.common;
+package com.blues.security.common;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/db")
+/**
+ * @author blues
+ */
+@RequestMapping("/database")
 public interface DatabaseApi {
 
+    /**
+     * hello
+     * @return
+     */
     @RequestMapping("hello")
     public String hello();
 
+
+    /**
+     * good
+     * @return
+     */
     @RequestMapping("good")
     public String good();
 }
--- common/src/main/java/com/blues/security/common/MiddleApi.java
+++ common/src/main/java/com/blues/security/common/MiddleApi.java
@@ -1,10 +1,17 @@
-package com.blues.common;
+package com.blues.security.common;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author blues
+ */
 @RequestMapping("/middle")
 public interface MiddleApi {
 
+    /**
+     * hello
+     * @return
+     */
     @RequestMapping("hello")
     public String hello();
 }
