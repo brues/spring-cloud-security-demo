edfe5f20cdec257dfabbfeaf932497c025a8e561
guoyibin <1979696310@qq.com>
2018-05-21 14:56:22 +0800

    注册中心

---
+++ eureka-server/src/main/java/com/blues/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.blues.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ *
+ *
+ * @author blues
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+}
